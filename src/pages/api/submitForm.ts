0f46a5fb01e59b319842f3b252cbf90b0e773406
nikunj309 <103521892+nikunj309@users.noreply.github.com>
2024-03-14 00:57:40 +0530

    first commit4

--- src/pages/api/submitForm.ts
+++ src/pages/api/submitForm.ts
@@ -1,6 +1,7 @@
 import nodemailer from 'nodemailer';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
       const formData = req.body;
